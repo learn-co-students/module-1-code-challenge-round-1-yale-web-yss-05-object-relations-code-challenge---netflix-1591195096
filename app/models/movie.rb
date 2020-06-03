ee655f1629028d9941ecd5b8858924f497e5c452
justin_james <jjames11799@gmail.com>
2020-06-03 12:26:25 -0400

    justinj code-challenge-1 done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,11 +5,36 @@ class Movie
 
   def initialize(title)
     @title = title
-    self.class.all << self
+    save 
+  end
+
+  def save
+    self.class.all << self 
   end
 
   def self.all
     @@all
+  end
+
+  def reviews
+    Review.all.select{|review| review.movie == self}
+  end
+
+  def reviewers
+    reviews.map{|review| review.viewer}
+  end
+# helper method for finding the average rating, 
+# adds up all ratings for a given movie
+  def total_ratings
+    reviews.sum{|review| review.rating}
+  end
+
+  def average_rating
+    self.total_ratings / reviews.count
+  end
+
+  def self.highest_rated
+    Movie.all.max{|movie_a, movie_b| movie_a.average_rating <=> movie_b.average_rating}
   end
 
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -5,11 +5,42 @@ class Viewer
 
   def initialize(username)
     @username = username
-    self.class.all << self
+    save 
+  end
+
+  def save
+    self.class.all << self 
   end
 
   def self.all
     @@all
   end
+
+  def reviews
+    Review.all.select{|review| review.viewer == self}
+  end
+
+  def reviewed_movies
+    reviews.map{|review| review.movie}
+  end
+
+  def reviewed_movie?(movie)
+    reviews.any? {|review| review.movie == movie}
+  end
+
+  def get_movie_review(movie)
+    reviews.detect{|review| review.movie == movie}
+  end
+
+  def rate_movie(movie, rating)
+    if reviewed_movie?(movie)
+      get_movie_review(movie).rating = rating 
+    else
+      Review.new(self, movie, rating)
+    end
+  end
+
+  
+
   
 end
