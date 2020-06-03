f49ad2b36c74b65bdaada96c1496f177d3344868
adilgondal1 <adil.gondal@yale.edu>
2020-06-03 11:45:50 -0400

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,26 @@ class Movie
     @@all
   end
 
+  def reviews
+    Review.all.select {|review| review.movie == self}
+  end
+
+  def reviewers
+    reviews.map {|review| review.viewer}
+  end
+
+  def average_rating
+    reviews.sum{|review| review.rating} / reviews.size.to_f
+  end
+
+  def self.highest_rated
+    self.all.max_by {|movie| movie.average_rating}
+  end
+
 end
+
+# Movie#average_rating
+# returns the average of all ratings for the Movie instance
+# to average ratings, add all ratings together and divide by the total number of ratings.
+# Movie.highest_rated
+# returns the Movie instance with the highest average rating.
--- tools/console.rb
+++ tools/console.rb
@@ -10,7 +10,18 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+v1 = Viewer.new("username1")
+v2 = Viewer.new("adil11111")
+v3 = Viewer.new("movieviewer123")
 
+m1 = Movie.new("Avatar")
+m2 = Movie.new("Taken")
+m3 = Movie.new("Miss Doubtfire")
+
+r1 = Review.new(v1,m1,9)
+r2 = Review.new(v2,m3,5)
+r3 = Review.new(v3,m2,7)
+r4 = Review.new(v1,m3,6)
 
 
 
