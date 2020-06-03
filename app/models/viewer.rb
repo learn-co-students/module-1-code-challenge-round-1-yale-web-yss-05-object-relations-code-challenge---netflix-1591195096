4140a133adda6c228357ee1daf3e91dbce252d78
robertalopez <roblop1234@yahoo.com>
2020-06-03 09:26:45 -0700

    Mod1 challenge

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,27 @@ class Viewer
   def self.all
     @@all
   end
+
+  def reviews
+    Review.all.select{|rev| rev.viewer == self}
+  end 
+
+  def reviewed_movies 
+    self.reviews.map{|rev| rev.movie}
+  end 
+
+  def reviewed_movie?(movie)
+    self.reviewed_movies.include?(movie)
+  end 
+
+  def rate_movie(movie, rating)
+    if reviewed_movie?(movie)
+      my_review = reviews.find{|rev| rev.movie == movie}
+      my_review.rating = rating 
+      my_review 
+    else 
+      Review.new(self, movie, rating)
+    end 
+  end 
   
 end
