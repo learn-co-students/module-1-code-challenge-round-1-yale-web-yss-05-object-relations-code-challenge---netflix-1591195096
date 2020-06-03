fe4515f12c4a2981232ccf6ed246d08ae66761e3
Joao Cardoso <joao.cardoso@yale.edu>
2020-06-03 12:15:30 -0400

    done

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,21 @@ class Viewer
   def self.all
     @@all
   end
+
+  def reviews
+    Review.all.select{|review| review.viewer == self}
+  end
+
+  def reviewed_movies
+    reviews.map{|review| review.movie}
+  end
+
+  def reviewed_movie?(movie)
+    reviewed_movies.include?(movie)
+  end
+
+  def rate_movie(movie, rating)
+    reviewed_movie?(movie) ? Review.all.find{|review| review.viewer == self && review.movie == movie}.rating = rating : Review.new(self, movie, rating)
+  end
   
 end
--- tools/console.rb
+++ tools/console.rb
@@ -9,7 +9,19 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+v1 = Viewer.new("v1")
+v2 = Viewer.new("v2")
+v3 = Viewer.new("v3")
 
+m1 = Movie.new("m1")
+m2 = Movie.new("m2")
+m3 = Movie.new("m3")
+
+r1 = Review.new(v1, m1, 10)
+r2 = Review.new(v1, m2, 5)
+r3 = Review.new(v2, m1, 9)
+r4 = Review.new(v3, m1, 8)
+r5 = Review.new(v1, m3, 10)
 
 
 
