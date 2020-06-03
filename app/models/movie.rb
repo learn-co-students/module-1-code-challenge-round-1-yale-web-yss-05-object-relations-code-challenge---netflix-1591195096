5f234948dd4b21fef877d476143b6b4742586044
dlee3427 <david.lee.sl2634@yale.edu>
2020-06-03 12:43:48 -0400

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie
-  attr_accessor :title
+  attr_accessor :title, :rating
 
   @@all = []
 
@@ -11,5 +11,23 @@ class Movie
   def self.all
     @@all
   end
+
+  def reviews 
+    Review.all.select {|r| r.movie == self }
+  end 
+
+  def reviewers 
+    reviews.map {|r| r.viewer }
+  end 
+
+  def average_rating 
+    num = reviews.map {|r| r.rating.to_f }.sum 
+    return num / reviews.count.to_f
+    # return num / self.rating.count
+  end 
+
+  def self.highest_rated 
+    self.all.max_by {|m| m.rating}
+  end 
 
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Viewer
   attr_accessor :username
 
@@ -11,5 +12,26 @@ class Viewer
   def self.all
     @@all
   end
-  
+
+  def reviews 
+    Review.all.select {|r| r.viewer == self}
+  end
+
+  def reviewed_movies 
+    reviews.map {|r| r.movie }
+  end 
+
+  def reviewed_movie?(movie)
+    self.reviews.include?(movie)
+  end 
+
+  def rate_movie(movie, rating)
+    if reviews.include?(self) && reviews.include?(movie)
+    # if reviews.any? {|r| r.viewer == self} && reviews.any? {|r| r.movie == movie}
+      self.rating == rating 
+    else
+      Review.new(self, movie, rating)
+    end 
+  end 
+
 end
