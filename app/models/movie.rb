15a31b731799fd6645692833584ee784384538c1
ev275 <enrique.valencia@yale.edu>
2020-06-03 11:23:52 -0500

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,35 @@ class Movie
     @@all
   end
 
+  def reviews
+    Review.all.select do |review|
+      review.movie == self
+    end
+  end
+
+  def reviewers
+    reviews.map do |review|
+      review.viewer
+    end.uniq
+  end
+
+  def ratings
+    reviews.map do |review|
+      review.rating
+    end
+  end
+
+  def average_rating
+    (ratings.sum).to_f / (ratings.size).to_f
+  end
+
+  def self.all_rated
+    all.select do |movie|
+      movie.reviews.size > 0
+    end
+  end
+
+  def self.highest_rated
+    self.all_rated.max { |movie1, movie2| movie1.average_rating <=> movie2.average_rating}
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,18 @@
 class Review
+    # check if any can be just reader
+    attr_reader :viewer, :movie
+    attr_accessor :rating
 
+    @@all = []
 
+    def initialize(viewer, movie, rating)
+        @viewer = viewer
+        @movie = movie
+        @rating = rating
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
