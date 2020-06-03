1942c7b6f3722393903fdf4fc0c06e18a6afa7ea
mlkelly <maurak87@gmail.com>
2020-06-03 12:26:45 -0400

    I did it.

--- tools/console.rb
+++ tools/console.rb
@@ -10,9 +10,17 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+v1=Viewer.new("Maura")
+v2=Viewer.new("Kelly")
 
+m1=Movie.new("Funny")
+m2=Movie.new("RomCom")
+m3=Movie.new("Horror")
 
-
+r1=Review.new(v1, m1, 10)
+r2=Review.new(v1, m2, 20)
+r3=Review.new(v2, m1, 30)
+r4=Review.new(v2, m3, 40)
 
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
 binding.pry
