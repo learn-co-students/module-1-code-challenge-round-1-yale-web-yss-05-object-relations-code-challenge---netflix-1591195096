0ec939afe29da1a6f9daa0e06f484b9f83b0cb59
Ziming Mao <ziming.mao@yale.edu>
2020-06-03 12:20:49 -0400

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,32 @@ class Movie
     @@all
   end
 
+  def reviews
+    Review.all.select{|review| review.movie == self}
+  end
+
+  def reviewers
+    reviews.map{|review| review.viewer}.uniq
+  end
+
+  def ratings
+    reviews.map{|review| review.rating}
+  end
+  def average_rating
+    ratings.sum() / ratings.length()
+  end
+
+  def self.highest_rated
+    # movie_max = @@all[0]
+    # max_rating = 0
+    # @@all.each do |movie|
+    #  if movie.average_rating > max_rating
+    #   max_rating = movie.average_rating
+    #   movie_max = movie
+    #  end
+    # end
+    # movie_max
+    @@all.max_by {|movie| movie.average_rating}
+  end
+
 end
