1964a53b8978bc2f8248a885fbcea323d32ec7c4
Anmolpreet <anmolpreet.kaur@yale.edu>
2020-06-03 12:28:43 -0400

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,21 @@ class Movie
     @@all
   end
 
+  def reviews 
+    Review.all.select {|review| review.movie == self}
+  end 
+
+  def reviewers 
+    self.reviews.map {|review| review.viewer}
+  end 
+
+  def average_rating 
+    sum = reviews.collect{|review| review.rating}.sum
+    sum / reviews.size.to_f
+  end 
+
+  def self.highest_rated
+    self.all.max_by {|movie| movie.average_rating}
+  end 
+
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,25 @@ class Viewer
   def self.all
     @@all
   end
+
+  def reviews 
+    Review.all.select {|review| review.viewer == self}
+  end 
+
+  def reviewed_movies 
+    self.reviews.map {|review| review.movie}
+  end 
+
+  def reviewed_movie?(movie)
+    reviewed_movies.include?(movie)
+  end 
+
+  def rate_movie(movie, rating)
+    if reviewed_movie?(movie) == true 
+      self.reviews.find{|review| review.movie == movie}.rating = rating 
+    else 
+      Review.new(viewer: self, movie: movie, rating: rating)
+    end 
+  end 
   
 end
