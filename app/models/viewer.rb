e240ec409e3527bc5dca9161112e618206487cff
Dennis <Dennis.portillo@yale.edu>
2020-06-03 09:38:34 -0700

    Dennis

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,15 +1,32 @@
 class Viewer
-  attr_accessor :username
+  attr_accessor :username, :rating
 
   @@all = []
 
   def initialize(username)
     @username = username
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
   end
+
+  def reviews
+    Review.all.select {|review| review.viewer == self}.uniq
+  end
   
+  def reviewed_movies
+    reviews.map {|revdmovies| revdmovies.movie}
+  end
+
+  def reviewed_movie?(movie)
+    reviewed_movies.include?(movie)
+  end
+
+  def rate_movie(movie, rating)
+    Review.new(self, movie, rating)
+  end
+
 end
+
--- tools/console.rb
+++ tools/console.rb
@@ -10,7 +10,23 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+m1 = Movie.new("Zootopia")
+m2 = Movie.new("Roma")
+m3 = Movie.new("Sparta")
+m4 = Movie.new("Avengers")
+m5 = Movie.new("La Luna")
 
+v1 = Viewer.new("Dennis")
+v2 = Viewer.new("Aud")
+v3 = Viewer.new("Geo")
+v4 = Viewer.new("Brian")
+v5 = Viewer.new("Jack")
+
+r1 = Review.new(v1, m1, 12)
+r2 = Review.new(v1, m4, 5)
+r3 = Review.new(v3, m5, 4)
+r4 = Review.new(v4, m2, 10)
+r5 = Review.new(v2, m1, 6)
 
 
 
