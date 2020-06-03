fcc1fb57370e5af170918efac125a4040409448c
Jonkavaliauskas <jonas.kavaliauskas@yale.edu>
2020-06-03 19:33:50 +0300

    ‘Done’

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,24 @@ class Movie
     @@all
   end
 
+  def reviews
+    Review.all.select{|review| review.movie == self}
+  end
+  
+  def reviewers
+    reviews.map{|review| review.viewer}
+  end
+
+  #a problem could arise, where the averaging two integers we get a float value
+  # and then it's automatically rounded down to a lower number. could attempt to solve
+  # this by turning ratings into floats beforehand, but not specified.
+  def average_rating
+    total_rating = reviews.map{|review| review.rating}.sum{|rating| rating}
+    average_rating = total_rating/reviews.length
+  end
+
+  def self.highest_rated
+    self.all.max_by{|movie| movie.average_rating}
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -11,7 +11,27 @@ end
 # so they will be available to test and play around with in your console
 
 
+# there's a lot that could be added to make this code neater. If the application was more extensive,
+# I would use modules, as the classes share similar functionality. However, because this particular assignment
+# is fairly short, it would be more hassle than benefit trying to include modules. Moreover, the task
+# doesn't specify how we should treat fringe cases (for example, now the same user can leave several reviews
+# for the same film, which should not be allower) - if this was a more elaborate project I was building,
+# I'd attempt to treat them. In the case that fringe cases would be present, additional
+# uniq should be added here and there.
 
+movie1 = Movie.new("Snatch")
+movie2 = Movie.new("Godfather")
+movie3 = Movie.new("Goodfellas")
+
+viewer1 = Viewer.new("Viewer1")
+viewer2 = Viewer.new("Viewer2")
+viewer3 = Viewer.new("Viewer3")
+
+review1 = Review.new(viewer1, movie1, 9.5)
+review2 = Review.new(viewer2, movie1, 8)
+review3 = Review.new(viewer3, movie2, 5)
+review4 = Review.new(viewer1, movie2, 10)
+review5 = Review.new(viewer3, movie3, 4)
 
 
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
