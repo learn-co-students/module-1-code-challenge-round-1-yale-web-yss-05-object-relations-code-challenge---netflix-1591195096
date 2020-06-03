3c4ab5fd8b0310da4ae76ec9e094b7e2a2c78872
s-dney <littlebirdsword@gmail.com>
2020-06-03 09:27:13 -0700

    done

--- tools/console.rb
+++ tools/console.rb
@@ -9,6 +9,45 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+v1 = Viewer.new("tarantinofan1992")
+v2 = Viewer.new("mothcub")
+v3 = Viewer.new("mrwiseauguy")
+v4 = Viewer.new("xX_D3M0N5L4Y3R_Xx")
+v5 = Viewer.new("floccinaucinihilipilification")
+
+pulp = Movie.new("Pulp Fiction")
+orange = Movie.new("A Clockwork Orange")
+arrival = Movie.new("Arrival")
+room = Movie.new("The Room")
+pan = Movie.new("Pan's Labyrinth")
+
+#I'm having my reviews be on a scale of 1 to 5 stars.
+
+v1.rate_movie(pulp, 5)
+v1.rate_movie(orange, 4)
+v1.rate_movie(arrival, 3.5)
+v1.rate_movie(room, 1)
+v1.rate_movie(pan, 4.5)
+
+v2.rate_movie(pulp, 3)
+v2.rate_movie(orange, 2)
+v2.rate_movie(arrival, 4)
+v2.rate_movie(room, 2)
+v2.rate_movie(pan, 5)
+
+v3.rate_movie(room, 5)
+
+v4.rate_movie(pulp, 4)
+v4.rate_movie(orange, 3.5)
+v4.rate_movie(arrival, 4.5)
+v4.rate_movie(room, 1)
+v4.rate_movie(pan, 5)
+
+v5.rate_movie(pulp, 4)
+v5.rate_movie(orange, 4.5)
+v5.rate_movie(arrival, 4)
+v5.rate_movie(room, 1.5)
+v5.rate_movie(pan, 4)
 
 
 
