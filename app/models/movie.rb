9e4af03bc091668c32cac02368501a0cc27dfbac
alexisteh <wenhui.teh23@gmail.com>
2020-06-03 11:21:12 -0500

    initial commit

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,11 +5,40 @@ class Movie
 
   def initialize(title)
     @title = title
-    self.class.all << self
+    self.class.all << self 
   end
 
   def self.all
     @@all
   end
 
+  def reviews 
+    #selects only reviews tied to this movie 
+    Review.all.select{|one_review| one_review.movie == self}
+  end 
+
+  def reviewers 
+    #turns list of reviews into list of associated reviewers
+    reviews.map(&:viewer).uniq 
+  end 
+
+  def average_rating 
+    #divides rating sum over number of reviews, making sure denominator is not 0
+    sum_ratings = reviews.sum{|one_review| one_review.rating}
+    if reviews.length != 0 
+      return sum_ratings / reviews.length
+    else return 0 
+    end 
+  end 
+
+  def self.highest_rated 
+    #returns any one movie instance with the highest possible average rating
+      self.all.max {|a, b| a.average_rating <=> b.average_rating}
+
+    # highest_possible_rating = self.all.max{|one_movie| one_movie.average_rating}.average_rating 
+    # movie_array_with_max_rating = self.all.select{|one_movie| one_movie.average_rating == highest_possible_rating}
+    # if movie_array_with_max_rating.size == 1 
+    #   return movie_array_with_max_rating[0]
+    # else return movie_array_with_max_rating
+  end 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -10,10 +10,35 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+view1 = Viewer.new("view1")
+view2 = Viewer.new("view2")
+view3 = Viewer.new("view3")
+view4 = Viewer.new("view3")
+
+
+mov1 = Movie.new("mov1")
+mov2 = Movie.new("mov2")
+mov3 = Movie.new("mov3")
+mov4 = Movie.new("mov4")
+
+
+rev1 = Review.new(view1, mov1, 14)
+rev2 = Review.new(view2, mov1,14)
+rev3 = Review.new(view4, mov1, 14)
+#mov1 has average rating 14 
+
+rev4 = Review.new(view3, mov2, 8)
+rev5 = Review.new(view2, mov2, 19)
+rev5 = Review.new(view1, mov2, 20)
+#mov2 has average rating 15.66666
+
+rev6 = Review.new(view2, mov3, 0)
+#mov3 has average rating 0
+
+#mov4 has average rating 0
 
 
 
-
-# ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
+# # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
 binding.pry
 0
