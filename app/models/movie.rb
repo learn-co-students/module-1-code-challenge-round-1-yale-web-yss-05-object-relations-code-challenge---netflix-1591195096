f86e4b20126aca24a5fa2e42ed255d4efdb5ff62
tbartelsmeyer <47281160+tbartelsmeyer@users.noreply.github.com>
2020-06-03 11:26:58 -0500

    Code Challenge Done 6.3

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,33 @@ class Movie
     @@all
   end
 
+  def reviews
+    Review.all.select { |rvw| rvw.movie == self }
+  end
+
+  def reviewers
+    self.reviews.map { |rvw| rvw.viewer }.uniq #I put a uniq here in case a viewer watched a movie twice
+  end
+
+  def average_rating
+    rtg_arr = self.reviews.map { |rvw| rvw.rating }
+    rtg_arr.sum / rtg_arr.length
+  end
+
+  def self.highest_rated
+    @@all.max {|a, b| a.average_rating <=> b.average_rating}
+  end
+
+  # OLD HIGHEST_RATED METHOD:
+  # def self.highest_rated
+  #   avg_rtg = 0
+  #   top_movie = "No movies have been rated"
+  #   @@all.each { |mv|
+  #     if mv.average_rating > avg_rtg
+  #       avg_rtg = mv.average_rating
+  #       top_movie = mv
+  #     end
+  #   }
+  #   top_movie
+  # end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -10,10 +10,50 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+mv1 = Movie.new("Humdingers Hoopty Doopty")
+mv2 = Movie.new("Star Wars")
+mv3 = Movie.new("Jaws")
 
+puts "\nMovie:"
+p "mv1: #{mv1} & #{mv1.title}"
+p "all movies: #{Movie.all}"
 
+vwr1 = Viewer.new("clint_clark")
+vwr2 = Viewer.new("CorgyDorgy")
+vwr3 = Viewer.new("St3v3Cargill")
 
+puts "\nViewer:"
+p "vwr1: #{vwr1} & #{vwr1.username}"
+p "all viewers: #{Viewer.all}"
+
+rvw1 = Review.new(vwr1,mv1,10)
+rvw2 = Review.new(vwr1,mv2,8)
+rvw3 = Review.new(vwr2,mv1,10)
+
+puts "\nReview:"
+p "rvw1: #{vwr1} & #{rvw1.viewer} & #{rvw1.movie} & #{rvw1.rating}"
+p "all reviews: #{Review.all}"
+puts "\n"
+
+p "mv1 reviews: #{mv1.reviews}"
+p "vwr1 reviews: #{vwr1.reviews}"
+puts "\n"
+p "mv1 reviewers: #{mv1.reviewers}"
+p "vwr1 reviewed movies: #{vwr1.reviewed_movies}"
+puts "\n"
+p "vwr1 reviewed_movie?(mv1): #{vwr1.reviewed_movie?(mv1)}"
+p "vwr1 reviewed_movie?(mv3): #{vwr1.reviewed_movie?(mv3)}"
+puts "\n"
+p "vwr1 rate_movie(mv1): #{vwr1.rate_movie(mv1,12)} (I mean seriously nothing can touch it)"
+p "vwr1 rate_movie(mv3): #{vwr1.rate_movie(mv3,7)}"
+puts "\n"
+p "mv1 average_rating: #{mv1.average_rating} (I mean SERIOUSLY nothing can touch it)"
+p mv3.reviews
+p "mv3 average_rating: #{mv3.average_rating}"
+
+puts "\n"
+p "top rated movie.title:  #{Movie.highest_rated.title} (why are we even asking at this point??)"
 
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
-binding.pry
+# binding.pry
 0
