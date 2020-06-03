5ced6af20f242035c033c314ff9e3bea9775c094
edensongjbs <edensongjbs@gmail.com>
2020-06-03 12:22:48 -0400

    completed code challenge

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,8 +8,24 @@ class Movie
     self.class.all << self
   end
 
+  def reviews
+    Review.all.select{|review| review.movie==self}
+  end
+
+  def reviewers
+    reviews.map{|review| review.viewer}.uniq
+  end
+
+  def average_rating # average will be float
+    reviews.reduce(0.0){|sum, review| sum+review.rating}/reviews.length
+  end
+
   def self.all
     @@all
+  end
+
+  def self.highest_rated
+    @@all.max{|movie1, movie2| movie1.average_rating <=> movie2.average_rating}
   end
 
 end
