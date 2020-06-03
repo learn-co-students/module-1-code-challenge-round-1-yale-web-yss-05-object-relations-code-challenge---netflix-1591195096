eb80841a3f3aac50144fd625708d3f1544c150c8
KalebGz <kaleb.zeray@gmail.com>
2020-06-03 12:30:17 -0400

    Done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,20 @@
 class Review
+    attr_reader :viewer, :movie
+    attr_accessor :rating
 
+    @@all = []
+
+    def initialize(viewer, movie, rating)
+        @viewer = viewer
+        @movie = movie
+        @rating = rating
+
+        self.class.all << self
+    end
+
+    # Class Methods
+    def self.all
+        @@all
+    end
 
 end
